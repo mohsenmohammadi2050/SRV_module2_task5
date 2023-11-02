c503015a5384081aeec814b6418187cf8be8084e
Mohsen Mohammadi <mohsenmohammadi2050@gmail.com>
2023-11-02 21:28:10 +0100

    first commit

---
+++ routes/json.js
@@ -0,0 +1,29 @@
+var express = require("express")
+var router = express.Router()
+var AWS = require("aws-sdk")
+var s3 = new AWS.S3()
+
+
+router.post("/", async function(req, res){
+    const { text } = req.body;
+    await s3.putObject({
+        Body: JSON.stringify({text:text}, null, 2),
+        Bucket: "cyclic-frightened-sarong-crab-eu-west-1",
+        Key: "text.json",
+    }).promise()
+    res.end()
+
+})
+
+
+router.get("/", async function(req, res){
+    let my_file = await s3.getObject({
+        Bucket: "cyclic-frightened-sarong-crab-eu-west-1",
+        Key: "text.json",
+    }).promise()
+
+    res.send(JSON.parse(my_file?.Body)?.text)
+})
+
+
+module.exports = router
